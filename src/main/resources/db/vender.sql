463d8a2fe3ee35f92f16b0f12c95ee5b378da946
dkask <dkaskgkdua@gmail.com>
2022-12-27 00:40:43 +0900

    DB 설계 - product, vender 설계

---
+++ src/main/resources/db/vender.sql
@@ -0,0 +1,11 @@
+create table vender
+(
+    id                 bigint auto_increment primary key,
+    vdr_nm         varchar(50)  not null comment '벤더명',
+    vd_sts             int(2)   default 0 comment '벤더 상태(0: 신청, 1: 심사중, 2: 승인, 3: 거절)',
+    created_date       datetime     null,
+    last_modified_date datetime     null,
+    created_by         bigint not null,
+    last_modified_by   bigint null
+)
+    engine = InnoDB;
