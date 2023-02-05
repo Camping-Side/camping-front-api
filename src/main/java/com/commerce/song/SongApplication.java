4bd568c20781422a8ff833c54c7142fd93bcca0d
dkask <dkaskgkdua@gmail.com>
2023-02-05 16:05:13 +0900

    fix: Spring Cloud Aws 배포 오류 수정

--- src/main/java/com/commerce/song/SongApplication.java
+++ src/main/java/com/commerce/song/SongApplication.java
@@ -3,6 +3,8 @@ package com.commerce.song;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.aws.autoconfigure.cache.ElastiCacheAutoConfiguration;
+import org.springframework.cloud.aws.autoconfigure.context.ContextInstanceDataAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -12,7 +14,12 @@ import java.util.Optional;
 import java.util.UUID;
 
 @EnableJpaAuditing
-@SpringBootApplication
+@SpringBootApplication(
+        exclude = {
+                ElastiCacheAutoConfiguration.class,
+                ContextInstanceDataAutoConfiguration.class,
+        }
+)
 public class SongApplication {
 
     public static void main(String[] args) {
